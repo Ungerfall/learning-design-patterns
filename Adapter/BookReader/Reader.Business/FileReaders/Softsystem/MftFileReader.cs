adfab380a1048d57d8453edc1c04d0b884ded578
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-08-09 20:59:42 +0400

    Adapt softsystem MftBook for IFileReader

---
+++ Adapter/BookReader/Reader.Business/FileReaders/Softsystem/MftFileReader.cs
@@ -0,0 +1,43 @@
+﻿using Reader.Interfaces;
+using Softsystem.Book;
+
+namespace Reader.Business.FileReaders.Softsystem
+{
+    public class MftFileReader : IFileReader
+    {
+        private MftBook book;
+        private int _currentPageIndex = 0;
+
+        public void LoadFile(string path)
+        {
+            book = new MftBook(path);
+            _currentPageIndex = 0;
+        }
+
+        public string NextPage => book.GetPage(++_currentPageIndex);
+
+        public string PreviousPage => book.GetPage(--_currentPageIndex);
+
+        public string FirstPage
+        {
+            get
+            {
+                _currentPageIndex = 0;
+                return book.GetPage(_currentPageIndex);
+            }
+        }
+
+        public string LastPage
+        {
+            get
+            {
+                _currentPageIndex = book.PageCount - 1;
+                return book.GetPage(_currentPageIndex);
+            }
+        }
+
+        public bool IsLastPage => _currentPageIndex == book.PageCount - 1;
+
+        public bool IsFirstPage => _currentPageIndex == 0;
+    }
+}
