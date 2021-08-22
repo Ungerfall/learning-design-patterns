5a3dff2dc7f54825dbca1b8712961686519802b9
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-08-22 20:25:02 +0400

    Support Deutsche bank

---
+++ AbstractFactory/RetailEquity/RetailEquity.Task2/DeutscheBankFactory.cs
@@ -0,0 +1,12 @@
+﻿using RetailEquity.Filters;
+
+namespace RetailEquity
+{
+    public class DeutscheBankFactory : IBankFactory
+    {
+        public IFilter CreateTradeFilter()
+        {
+            return new DeutscheBankFilter();
+        }
+    }
+}
---
+++ AbstractFactory/RetailEquity/RetailEquity.Task2/Filters/DeutscheBankFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RetailEquity.Model;
+
+namespace RetailEquity.Filters
+{
+    public class DeutscheBankFilter : IFilter
+    {
+        public IEnumerable<Trade> Match(IEnumerable<Trade> trades)
+        {
+            const int amountMin = 90;
+            const int amountMax = 120;
+            return trades.Where(x =>
+                x.Type == TradeType.Option
+                && x.SubType == TradeSubType.NewOption
+                && x.Amount > amountMin
+                && x.Amount < amountMax);
+        }
+    }
+}
