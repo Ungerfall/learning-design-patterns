21327f7589d3c293ebe7a40cfc5d876a2ca472a6
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-08-22 21:44:20 +0400

    Add england branch for Barclays

---
+++ AbstractFactory/RetailEquity/RetailEquity.Task3/BarclaysEnglandBankFactory.cs
@@ -0,0 +1,12 @@
+﻿using RetailEquity.Filters;
+
+namespace RetailEquity
+{
+    public class BarclaysEnglandBankFactory : IBankFactory
+    {
+        public IFilter CreateTradeFilter()
+        {
+            return new BarclaysEnglandBankFilter();
+        }
+    }
+}
---
+++ AbstractFactory/RetailEquity/RetailEquity.Task3/Filters/BarclaysEnglandBankFilter.cs
@@ -0,0 +1,15 @@
+﻿using RetailEquity.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RetailEquity.Filters
+{
+    public class BarclaysEnglandBankFilter : IFilter
+    {
+        public IEnumerable<Trade> Match(IEnumerable<Trade> trades)
+        {
+            const int amountMin = 100;
+            return trades.Where(x => x.Type == TradeType.Future && x.Amount > amountMin);
+        }
+    }
+}
