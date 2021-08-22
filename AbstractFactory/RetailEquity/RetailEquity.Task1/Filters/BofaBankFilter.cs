4e64aa45bb91373458389f1e7f5608092b90c725
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-08-22 20:04:42 +0400

    Add bank filters

---
+++ AbstractFactory/RetailEquity/RetailEquity.Task1/Filters/BofaBankFilter.cs
@@ -0,0 +1,15 @@
+﻿using RetailEquity.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RetailEquity.Filters
+{
+    public class BofaBankFilter : IFilter
+    {
+        public IEnumerable<Trade> Match(IEnumerable<Trade> trades)
+        {
+            const int amountMin = 70;
+            return trades.Where(x => x.Amount > amountMin);
+        }
+    }
+}
---
+++ AbstractFactory/RetailEquity/RetailEquity.Task1/Filters/ConnacordBankFilter.cs
@@ -0,0 +1,16 @@
+﻿using RetailEquity.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RetailEquity.Filters
+{
+    public class ConnacordBankFilter : IFilter
+    {
+        public IEnumerable<Trade> Match(IEnumerable<Trade> trades)
+        {
+            const int amountMin = 10;
+            const int amountMax = 40;
+            return trades.Where(x => x.Type == TradeType.Future && x.Amount > amountMin && x.Amount < amountMax);
+        }
+    }
+}
