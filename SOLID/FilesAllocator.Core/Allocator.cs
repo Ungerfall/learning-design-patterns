3594ddf7d09369ef138be7307439d04e7ed24c4b
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-09-12 20:47:01 +0400

    Add interface for allocator

--- SOLID/FilesAllocator.Core/Allocator.cs
+++ SOLID/FilesAllocator.Core/Allocator.cs
@@ -1,216 +1,204 @@
-﻿using System;
+﻿using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using MetadataExtractor;
-using MetadataExtractor.Formats.Exif;
 
 namespace FilesAllocator.Core
 {
-	public class Allocator
-	{
-		#region Fields
+    public class Allocator : IAllocator
+    {
+        private List<ProcessedFile> _files = new List<ProcessedFile>();
 
-		private List<ProcessedFile> _files = new List<ProcessedFile>();
+        /// <summary>
+        /// Copying files from inputDirectory to outputDirectory
+        /// </summary>
+        /// <param name="inputDirectory">Input directory</param>
+        /// <param name="outputDirectory">Output directory</param>
+        /// <param name="useSubFolders">Copying files from sub-directories</param>
+        /// <param name="creationDateTimePrefixName">TRUE - if you need to add the creation date of file as prefix for filename</param>
+        /// <param name="groupByCreationDateHandler">TRUE - if you need to group files into directories by the creation date</param>
+        /// <param name="filteredExtensions">Array of type extensions which should be copying</param>
+        /// <returns>Count of copied files</returns>
+        public int Copy(string inputDirectory,
+            string outputDirectory,
+            bool useSubFolders,
+            bool creationDateTimePrefixName = false,
+            bool groupByCreationDateHandler = false,
+            string[] filteredExtensions = null)
+        {
+            var inputFiles = GetFilesByDirectory(inputDirectory, useSubFolders);
+            _files = inputFiles.Select(f => new ProcessedFile(f)).ToList();
 
-		#endregion
+            if (creationDateTimePrefixName)
+            {
+                AddCreationDatePrefixForFilename();
+            }
 
-		#region Public
+            if (groupByCreationDateHandler)
+            {
+                GroupByCreationDate();
+            }
 
-		/// <summary>
-		/// Copying files from inputDirectory to outputDirectory
-		/// </summary>
-		/// <param name="inputDirectory">Input directory</param>
-		/// <param name="outputDirectory">Output directory</param>
-		/// <param name="useSubFolders">Copying files from sub-directories</param>
-		/// <param name="creationDateTimePrefixName">TRUE - if you need to add the creation date of file as prefix for filename</param>
-		/// <param name="groupByCreationDateHandler">TRUE - if you need to group files into directories by the creation date</param>
-		/// <param name="filteredExtensions">Array of type extensions which should be copying</param>
-		/// <returns>Count of copied files</returns>
-		public int Copy(string inputDirectory,
-			string outputDirectory,
-			bool useSubFolders, 
-			bool creationDateTimePrefixName = false,
-			bool groupByCreationDateHandler = false,
-			string[] filteredExtensions = null)
-		{
-			var inputFiles = GetFilesByDirectory(inputDirectory, useSubFolders);
-			_files = inputFiles.Select(f => new ProcessedFile(f)).ToList();
+            if (filteredExtensions?.Length > 0)
+            {
+                FilteredExtensions(filteredExtensions);
+            }
 
-			if (creationDateTimePrefixName)
-			{
-				AddCreationDatePrefixForFilename();
-			}
+            RenameDuplicateFiles();
 
-			if (groupByCreationDateHandler)
-			{
-				GroupByCreationDate();
-			}
+            var result = 0;
+            foreach (var file in _files)
+            {
+                var newFullName = Path.Combine(outputDirectory, file.EndpointFullName);
 
-			if (filteredExtensions?.Length > 0)
-			{
-				FilteredExtensions(filteredExtensions);
-			}
+                try
+                {
+                    CopyFile(file.InitFullName, newFullName);
+                    result++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
 
-			RenameDuplicateFiles();
+            return result;
+        }
 
-			var result = 0;
-			foreach (var file in _files)
-			{
-				var newFullName = Path.Combine(outputDirectory, file.EndpointFullName);
+        /// <summary>
+        /// Get file list by path
+        /// </summary>
+        /// <param name="path">Path</param>
+        /// <param name="useSubDirectories">Find files in sub-directories</param>
+        /// <returns>List of files</returns>
+        public IEnumerable<string> GetFilesByDirectory(string path, bool useSubDirectories = false)
+        {
+            var files = System.IO.Directory.GetFiles(path).ToList();
 
-				try
-				{
-					CopyFile(file.InitFullName, newFullName);
-					result++;
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine(ex.Message);
-				}
-			}
+            if (useSubDirectories)
+            {
+                foreach (var dir in System.IO.Directory.GetDirectories(path))
+                {
+                    files.AddRange(GetFilesByDirectory(dir, true));
+                }
+            }
 
-			return result;
-		}
+            return files;
+        }
 
-		/// <summary>
-		/// Get file list by path
-		/// </summary>
-		/// <param name="path">Path</param>
-		/// <param name="useSubDirectories">Find files in sub-directories</param>
-		/// <returns>List of files</returns>
-		public IEnumerable<string> GetFilesByDirectory(string path, bool useSubDirectories = false)
-		{
-			var files = System.IO.Directory.GetFiles(path).ToList();
+        /// <summary>
+        /// Copy file from source to destination
+        /// </summary>
+        /// <param name="sourceFileName">Source filename</param>
+        /// <param name="destFileName">Destination filename</param>
+        public void CopyFile(string sourceFileName, string destFileName)
+        {
+            var destDirectory = Path.GetDirectoryName(destFileName);
 
-			if (useSubDirectories)
-			{
-				foreach (var dir in System.IO.Directory.GetDirectories(path))
-				{
-					files.AddRange(GetFilesByDirectory(dir, true));
-				}
-			}
+            if (destDirectory == null)
+            {
+                throw new NullReferenceException("Cannot get destination directory.");
+            }
 
-			return files;
-		}
+            if (!System.IO.Directory.Exists(destDirectory))
+            {
+                System.IO.Directory.CreateDirectory(destDirectory);
+            }
 
-		/// <summary>
-		/// Copy file from source to destination
-		/// </summary>
-		/// <param name="sourceFileName">Source filename</param>
-		/// <param name="destFileName">Destination filename</param>
-		public void CopyFile(string sourceFileName, string destFileName)
-		{
-			var destDirectory = Path.GetDirectoryName(destFileName);
+            File.Copy(sourceFileName, destFileName, true);
+        }
 
-			if (destDirectory == null)
-			{
-				throw new NullReferenceException("Cannot get destination directory.");
-			}
+        /// <summary>
+        /// Add GUID as postfix of filename for duplicated files
+        /// </summary>
+        private void RenameDuplicateFiles()
+        {
+            foreach (var file in _files)
+            {
+                var hasDuplicate = _files.Count(f => f.EndpointFullName.Equals(file.EndpointFullName)) > 1;
+                if (!hasDuplicate) continue;
 
-			if (!System.IO.Directory.Exists(destDirectory))
-			{
-				System.IO.Directory.CreateDirectory(destDirectory);
-			}
+                var fileName = file.Name;
+                var uniqueString = $"_{Guid.NewGuid():N}";
+                var uniqueFileName = fileName.Insert(fileName.Length - file.FileInfo.Extension.Length, uniqueString);
+                file.Name = uniqueFileName;
+            }
+        }
 
-			File.Copy(sourceFileName, destFileName, true);
-		}
+        /// <summary>
+        /// Add the creation date of file as prefix for filename
+        /// </summary>
+        private void AddCreationDatePrefixForFilename()
+        {
+            foreach (var file in _files)
+            {
+                DateTime? dateTaken;
+                try
+                {
+                    var directories = ImageMetadataReader.ReadMetadata(file.FileInfo.FullName);
+                    var subIfdDirectory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
+                    dateTaken = subIfdDirectory?.GetDateTime(ExifDirectoryBase.TagDateTimeOriginal);
+                }
+                catch (ImageProcessingException)
+                {
+                    dateTaken = null;
+                }
 
-		#endregion
+                var createDateTime = dateTaken ?? file.FileInfo.CreationTime;
+                var createDate = createDateTime.ToString("yyyyMMdd");
+                var createTime = createDateTime.ToString("HHmmss");
+                file.Name = $"{createDate}-{createTime}_{file.Name}";
+            }
+        }
 
-		#region Helper methods
+        /// <summary>
+        /// Group files into directories by the creation date
+        /// </summary>
+        private void GroupByCreationDate()
+        {
+            foreach (var file in _files)
+            {
+                DateTime? dateTaken;
+                try
+                {
+                    var directories = ImageMetadataReader.ReadMetadata(file.FileInfo.FullName);
+                    var subIfdDirectory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
+                    dateTaken = subIfdDirectory?.GetDateTime(ExifDirectoryBase.TagDateTimeOriginal);
+                }
+                catch (ImageProcessingException)
+                {
+                    dateTaken = null;
+                }
 
-		/// <summary>
-		/// Add GUID as postfix of filename for duplicated files
-		/// </summary>
-		private void RenameDuplicateFiles()
-		{
-			foreach (var file in _files)
-			{
-				var hasDuplicate = _files.Count(f => f.EndpointFullName.Equals(file.EndpointFullName)) > 1;
-				if (!hasDuplicate) continue;
+                var createDateTime = dateTaken ?? file.FileInfo.CreationTime;
+                var newFilePath = createDateTime.ToString("yyyy-MM-dd");
 
-				var fileName = file.Name;
-				var uniqueString = $"_{Guid.NewGuid():N}";
-				var uniqueFileName = fileName.Insert(fileName.Length - file.FileInfo.Extension.Length, uniqueString);
-				file.Name = uniqueFileName;
-			}
-		}
+                file.EndpointDirectory = Path.Combine(
+                    file.EndpointDirectory ?? string.Empty,
+                    newFilePath);
+            }
+        }
 
-		/// <summary>
-		/// Add the creation date of file as prefix for filename
-		/// </summary>
-		private void AddCreationDatePrefixForFilename()
-		{
-			foreach (var file in _files)
-			{
-				DateTime? dateTaken;
-				try
-				{
-					var directories = ImageMetadataReader.ReadMetadata(file.FileInfo.FullName);
-					var subIfdDirectory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
-					dateTaken = subIfdDirectory?.GetDateTime(ExifDirectoryBase.TagDateTimeOriginal);
-				}
-				catch (ImageProcessingException)
-				{
-					dateTaken = null;
-				}
+        /// <summary>
+        /// Filter files by extensions
+        /// </summary>
+        /// <param name="extensions"></param>
+        private void FilteredExtensions(string[] extensions)
+        {
+            var removingFiles = _files
+                .Where(f =>
+                {
+                    var fileExtension = f.FileInfo.Extension.Replace(".", string.Empty);
+                    return !extensions.Any(e => e.ToUpper().Equals(fileExtension.ToUpper()));
+                })
+                .ToArray();
 
-				var createDateTime = dateTaken ?? file.FileInfo.CreationTime;
-				var createDate = createDateTime.ToString("yyyyMMdd");
-				var createTime = createDateTime.ToString("HHmmss");
-				file.Name = $"{createDate}-{createTime}_{file.Name}";
-			}
-		}
-
-		/// <summary>
-		/// Group files into directories by the creation date
-		/// </summary>
-		private void GroupByCreationDate()
-		{
-			foreach (var file in _files)
-			{
-				DateTime? dateTaken;
-				try
-				{
-					var directories = ImageMetadataReader.ReadMetadata(file.FileInfo.FullName);
-					var subIfdDirectory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
-					dateTaken = subIfdDirectory?.GetDateTime(ExifDirectoryBase.TagDateTimeOriginal);
-				}
-				catch (ImageProcessingException)
-				{
-					dateTaken = null;
-				}
-
-				var createDateTime = dateTaken ?? file.FileInfo.CreationTime;
-				var newFilePath = createDateTime.ToString("yyyy-MM-dd");
-
-				file.EndpointDirectory = Path.Combine(
-					file.EndpointDirectory ?? string.Empty,
-					newFilePath);
-			}
-		}
-
-		/// <summary>
-		/// Filter files by extensions
-		/// </summary>
-		/// <param name="extensions"></param>
-		private void FilteredExtensions(string[] extensions)
-		{
-			var removingFiles = _files
-				.Where(f =>
-				{
-					var fileExtension = f.FileInfo.Extension.Replace(".", string.Empty);
-					return !extensions.Any(e => e.ToUpper().Equals(fileExtension.ToUpper()));
-				})
-				.ToArray();
-
-			foreach (var file in removingFiles)
-			{
-				_files.Remove(file);
-			}
-		}
-
-		#endregion
-	}
+            foreach (var file in removingFiles)
+            {
+                _files.Remove(file);
+            }
+        }
+    }
 }
---
+++ SOLID/FilesAllocator.Core/IAllocator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace FilesAllocator.Core
+{
+    public interface IAllocator
+    {
+        int Copy(string inputDirectory,
+            string outputDirectory,
+            bool useSubFolders,
+            bool creationDateTimePrefixName = false,
+            bool groupByCreationDateHandler = false,
+            string[] filteredExtensions = null);
+
+        void CopyFile(string sourceFileName, string destFileName);
+
+        IEnumerable<string> GetFilesByDirectory(string path, bool useSubDirectories = false);
+    }
+}
