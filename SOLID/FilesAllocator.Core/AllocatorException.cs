eda70129a23abde6346423de2e0ff2724079db8e
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-09-19 19:38:40 +0400

    Wrap all exceptions in AllocatorException

---
+++ SOLID/FilesAllocator.Core/AllocatorException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FilesAllocator.Core
+{
+    public class AllocatorException : Exception
+    {
+        public AllocatorException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ SOLID/FilesAllocator.Core/FileCopier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilesAllocator.Core
+{
+    internal class FileCopier : IFileCopier
+    {
+        public int Copy(ICollection<File> files)
+        {
+            var result = 0;
+            foreach (var file in files)
+            {
+                CopyFile(file.InitFullName, file.EndpointFullName);
+                result++;
+            }
+
+            return result;
+        }
+
+        internal static void CopyFile(string sourceFileName, string destFileName)
+        {
+            var destDirectory = Path.GetDirectoryName(destFileName);
+
+            if (destDirectory == null)
+            {
+                throw new NullReferenceException("Cannot get destination directory.");
+            }
+
+            if (!Directory.Exists(destDirectory))
+            {
+                Directory.CreateDirectory(destDirectory);
+            }
+
+            System.IO.File.Copy(sourceFileName, destFileName, overwrite: true);
+        }
+    }
+}
